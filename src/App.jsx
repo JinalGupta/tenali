bf28f397b02a259d7e6d0a591159ff8943ea0559
Jinal Gupta <jinalgupta@gmail.com>
2026-05-14 10:30:23 +0000

    Use HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import AppRoutes from './routes'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="min-h-screen bg-navy-950 flex items-center justify-center">
         <AppRoutes />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
